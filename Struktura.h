632b768d1c9fda0d63b76983f5785f622b06798f
Gustavas <gustavas.marcinkevicius@gmail.com>
2025-04-06 14:55:31 +0300

    class is just public for now

--- Struktura.h
+++ Struktura.h
@@ -6,9 +6,8 @@
 using std::string;
 using std::vector;
 
-
 class Studentas {
-    private:
+    public:
         std::string vardas;
         std::string pavarde;
         std::vector<int> pazymiai;
@@ -16,55 +15,7 @@ class Studentas {
         double GalutinisBalasVidurkis;
         double GalutinisBalasMediana;
     
-    public:
-        Studentas(const std::string& vardas, const std::string& pavarde,
-                  const std::vector<int>& pazymiai, int egzaminoPazimys)
-            : vardas(vardas), pavarde(pavarde), pazymiai(pazymiai), egzaminoPazimys(egzaminoPazimys) {
-            GalutinisBalasVidurkis = calculateFinalGradeAverage();
-            GalutinisBalasMediana = calculateFinalGradeMedian();
-        }
-    
-        std::string getVardas() const { return vardas; }
-        std::string getPavarde() const { return pavarde; }
-        double getGalutinisBalasVidurkis() const { return GalutinisBalasVidurkis; }
-        double getGalutinisBalasMediana() const { return GalutinisBalasMediana; }
-   
-        double calculateFinalGradeAverage() {
-            double avg = 0.0;
-            for (int grade : pazymiai) {
-                avg += grade;
-            }
-            if (!pazymiai.empty()) {
-                avg /= pazymiai.size();
-            }
-            return 0.4 * avg + 0.6 * egzaminoPazimys;
-        }
-    
-        double calculateFinalGradeMedian() {
-            if (pazymiai.empty()) return 0.0;
-            std::vector<int> sortedGrades = pazymiai;
-            std::sort(sortedGrades.begin(), sortedGrades.end());
-            int n = sortedGrades.size();
-            double median = 0.0;
-            if (n % 2 == 0) {
-                median = (sortedGrades[n / 2 - 1] + sortedGrades[n / 2]) / 2.0;
-            } else {
-                median = sortedGrades[n / 2];
-            }
-            return 0.4 * median + 0.6 * egzaminoPazimys;
-        }
-    
-     
-        void printStudent() const {
-            std::cout << std::left << std::setw(25) << vardas
-                      << std::setw(25) << pavarde;
-            for (int grade : pazymiai) {
-                std::cout << std::setw(6) << grade;
-            }
-            std::cout << std::setw(6) << egzaminoPazimys << "\n";
-        }
-    };
-    
+        Studentas() : vardas(""), pavarde(""), egzaminoPazimys(0), GalutinisBalasVidurkis(0.0), GalutinisBalasMediana(0.0) {}
+};
 
 #endif 
-
