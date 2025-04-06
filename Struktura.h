e3180048f72d1ce8635487dc744e075f6642d0d8
Gustavas <gustavas.marcinkevicius@gmail.com>
2025-04-06 14:32:50 +0300

    created class

--- Struktura.h
+++ Struktura.h
@@ -7,14 +7,23 @@ using std::string;
 using std::vector;
 
 
-struct Studentas {
-    string vardas;
-    string pavarde;
-    vector<int> pazymiai;  
-    int egzaminoPazimys;
-    double GalutinisBalasVidurkis;
-    double GalutinisBalasMediana;
-};
+class Studentas {
+    private:
+        std::string vardas_;
+        std::string pavarde_;
+        double egzaminas_;
+        std::vector<double> nd_;
+    
+    public:
+        Studentas() : egzaminas_(0) { }
+    
+        Studentas(std::istream& is);
+    
+        inline std::string vardas() const { return vardas_; }
+    
+        inline std::string pavarde() const { return pavarde_; }
+   
+    };
 
 #endif 
 
